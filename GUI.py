fd081c6772508987f7d6912a90dd8d202a63647f
Mronanmag <49346195+Mronanmag@users.noreply.github.com>
2023-10-24 17:12:37 +0200

    Add files via upload
    Ajout Widget Résolution (pas fini)

--- GUI.py
+++ GUI.py
@@ -1,3 +1,6 @@
+from Resolution import FastaFindSpecies
+from Resolution import Request
+from Resolution import FileCharge
 from librairie import *
 
 class PrimerMain(QMainWindow):
@@ -33,6 +36,8 @@ class PrimerMain(QMainWindow):
 		self.display_text.setLineWrapMode(QTextEdit.NoWrap)
 		self.display_text.setFontFamily("monospace")
 
+		self.resolution_widget = ResolutionWidget()
+		
 
 		top_bar = QWidget()
 		top_bar_layout = QHBoxLayout()
@@ -53,10 +58,11 @@ class PrimerMain(QMainWindow):
 		top_bar_layout.addWidget(title_label)
 
 		main_layout = QGridLayout()
-		main_layout.addWidget(top_bar, 0, 0, 1, 2)
-		main_layout.addWidget(self.form, 1, 0)
+		main_layout.addWidget(top_bar, 0,0,1,1)
+		main_layout.addWidget(self.form, 1, 0,1,1)
 		#La taille du display text doit être agrandie et correspondre à 2/3 de la fenetre
-		main_layout.addWidget(self.display_text, 1, 1)
+		main_layout.addWidget(self.display_text, 1, 1,4,4)
+		main_layout.addWidget(self.resolution_widget,2,0,1,1)
 
 		main_widget = QWidget()
 		main_widget.setLayout(main_layout)
@@ -99,6 +105,77 @@ class PrimerMain(QMainWindow):
 
 	def cleanDisplayText(self):
 		self.display_text.clear()
+
+class ResolutionWidget(QWidget) :
+	def __init__(self,parent=None):
+		super().__init__(parent)
+		self.chargeFile()
+		self.widget()
+		self.layout()
+
+	def chargeFile(self):
+		self.names = FileCharge()
+		self.nodes = FileCharge()
+		self.names.ChargerNames()
+		self.nodes.ChargerNodes()
+
+	def widget(self):
+		self.lb_choice = QLabel("Quel est le rang taxonomique recherché")
+		self.le_choice = QLineEdit()
+		self.lb_fasta = QLabel("Fichier fasta généré : ")
+		self.le_fasta = QLineEdit()
+		self.btn_fasta = QPushButton("Find")
+		self.lb_resolution = QLabel("Taux de résolution")
+		self.le_resolution = QLineEdit()
+		self.btn_calculate = QPushButton('Calculate')
+
+	def layout(self):
+		self.main_layout = QVBoxLayout()
+		self.layout_querie = QHBoxLayout()
+		self.layout_querie.addWidget(self.lb_choice)
+		self.layout_querie.addWidget(self.le_choice)
+		self.layout_fichier = QHBoxLayout()
+		self.layout_fichier.addWidget(self.lb_fasta)
+		self.layout_fichier.addWidget(self.le_fasta)
+		self.layout_fichier.addWidget(self.btn_fasta)
+		self.layout_resolution = QHBoxLayout()
+		self.layout_resolution.addWidget(self.lb_resolution)
+		self.layout_resolution.addWidget(self.le_resolution)
+		self.layout_resolution.addWidget(self.btn_calculate)
+		self.main_layout.addLayout(self.layout_querie)	
+		self.main_layout.addLayout(self.layout_fichier)
+		self.main_layout.addLayout(self.layout_resolution)
+		self.setLayout(self.main_layout)
+
+
+	def getChoice(self) :
+		if isinstance(self.le_choice.text(), str) :
+			return self.le_choice.text()
+		return False
+
+	def getFile(self) :
+		return self.le_fasta.text()	
+
+	def connect(self) :
+		pass
+
+	def calculate(self) :
+		rq = Request(self.name.fichier,self.nodes.fichier)
+		querie = self.getChoice()		
+		if querie :
+			rq.setQuerie(querie)
+			rq.setSpeciesList()			
+			ffs = FastaFindSpecies(self.getFile())
+			ffs.compare2list(rq.species_list_therique,ffs.liste_species_fasta)
+			
+
+    # def browse_file(self):
+    #     options = QFileDialog.Options()
+    #     options |= QFileDialog.ReadOnly
+    #     file_name, _ = QFileDialog.getOpenFileName(self, "Ouvrir un fichier", "", "All Files (*);;Python Files (*.py)",
+    #                                                options=options)
+    #     if file_name:
+    #         pass
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
